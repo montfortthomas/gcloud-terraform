a717b45de5ec627d4a692797319c7feda1789658
montfortthomas <85698949+montfortthomas@users.noreply.github.com>
2022-02-04 08:38:40 +0530

    Update bucket.tf

--- Ford-Data-Migration-POC/bucket.tf
+++ Ford-Data-Migration-POC/bucket.tf
@@ -33,3 +33,454 @@ resource "google_storage_bucket" "s3" {
     storage_class               = "STANDARD"
     uniform_bucket_level_access = false
 }
+resource "google_storage_bucket" "s4" {
+    default_event_based_hold    = false
+    force_destroy               = false
+    labels                      = {}
+    location                    = "AUSTRALIA-SOUTHEAST1"
+    name                        = "${var.bucket}_s3"
+    project                     = "ford-data-migration-poc"
+    requester_pays              = false
+    storage_class               = "STANDARD"
+    uniform_bucket_level_access = false
+}
+resource "google_storage_bucket" "s5" {
+    default_event_based_hold    = false
+    force_destroy               = false
+    labels                      = {}
+    location                    = "AUSTRALIA-SOUTHEAST1"
+    name                        = "${var.bucket}_s3"
+    project                     = "ford-data-migration-poc"
+    requester_pays              = false
+    storage_class               = "STANDARD"
+    uniform_bucket_level_access = false
+}
+resource "google_storage_bucket" "s6" {
+    default_event_based_hold    = false
+    force_destroy               = false
+    labels                      = {}
+    location                    = "AUSTRALIA-SOUTHEAST1"
+    name                        = "${var.bucket}_s3"
+    project                     = "ford-data-migration-poc"
+    requester_pays              = false
+    storage_class               = "STANDARD"
+    uniform_bucket_level_access = false
+}
+resource "google_storage_bucket" "s7" {
+    default_event_based_hold    = false
+    force_destroy               = false
+    labels                      = {}
+    location                    = "AUSTRALIA-SOUTHEAST1"
+    name                        = "${var.bucket}_s3"
+    project                     = "ford-data-migration-poc"
+    requester_pays              = false
+    storage_class               = "STANDARD"
+    uniform_bucket_level_access = false
+}
+resource "google_storage_bucket" "s8" {
+    default_event_based_hold    = false
+    force_destroy               = false
+    labels                      = {}
+    location                    = "AUSTRALIA-SOUTHEAST1"
+    name                        = "${var.bucket}_s3"
+    project                     = "ford-data-migration-poc"
+    requester_pays              = false
+    storage_class               = "STANDARD"
+    uniform_bucket_level_access = false
+}
+resource "google_storage_bucket" "s9" {
+    default_event_based_hold    = false
+    force_destroy               = false
+    labels                      = {}
+    location                    = "AUSTRALIA-SOUTHEAST1"
+    name                        = "${var.bucket}_s3"
+    project                     = "ford-data-migration-poc"
+    requester_pays              = false
+    storage_class               = "STANDARD"
+    uniform_bucket_level_access = false
+}
+resource "google_storage_bucket" "s10" {
+    default_event_based_hold    = false
+    force_destroy               = false
+    labels                      = {}
+    location                    = "AUSTRALIA-SOUTHEAST1"
+    name                        = "${var.bucket}_s3"
+    project                     = "ford-data-migration-poc"
+    requester_pays              = false
+    storage_class               = "STANDARD"
+    uniform_bucket_level_access = false
+}
+resource "google_storage_bucket" "s11" {
+    default_event_based_hold    = false
+    force_destroy               = false
+    labels                      = {}
+    location                    = "AUSTRALIA-SOUTHEAST1"
+    name                        = "${var.bucket}_s3"
+    project                     = "ford-data-migration-poc"
+    requester_pays              = false
+    storage_class               = "STANDARD"
+    uniform_bucket_level_access = false
+}
+resource "google_storage_bucket" "s12" {
+    default_event_based_hold    = false
+    force_destroy               = false
+    labels                      = {}
+    location                    = "AUSTRALIA-SOUTHEAST1"
+    name                        = "${var.bucket}_s3"
+    project                     = "ford-data-migration-poc"
+    requester_pays              = false
+    storage_class               = "STANDARD"
+    uniform_bucket_level_access = false
+}
+resource "google_storage_bucket" "s13" {
+    default_event_based_hold    = false
+    force_destroy               = false
+    labels                      = {}
+    location                    = "AUSTRALIA-SOUTHEAST1"
+    name                        = "${var.bucket}_s3"
+    project                     = "ford-data-migration-poc"
+    requester_pays              = false
+    storage_class               = "STANDARD"
+    uniform_bucket_level_access = false
+}
+resource "google_storage_bucket" "s14" {
+    default_event_based_hold    = false
+    force_destroy               = false
+    labels                      = {}
+    location                    = "AUSTRALIA-SOUTHEAST1"
+    name                        = "${var.bucket}_s3"
+    project                     = "ford-data-migration-poc"
+    requester_pays              = false
+    storage_class               = "STANDARD"
+    uniform_bucket_level_access = false
+}
+resource "google_storage_bucket" "s15" {
+    default_event_based_hold    = false
+    force_destroy               = false
+    labels                      = {}
+    location                    = "AUSTRALIA-SOUTHEAST1"
+    name                        = "${var.bucket}_s3"
+    project                     = "ford-data-migration-poc"
+    requester_pays              = false
+    storage_class               = "STANDARD"
+    uniform_bucket_level_access = false
+}
+resource "google_storage_bucket" "s16" {
+    default_event_based_hold    = false
+    force_destroy               = false
+    labels                      = {}
+    location                    = "AUSTRALIA-SOUTHEAST1"
+    name                        = "${var.bucket}_s3"
+    project                     = "ford-data-migration-poc"
+    requester_pays              = false
+    storage_class               = "STANDARD"
+    uniform_bucket_level_access = false
+}
+resource "google_storage_bucket" "s17" {
+    default_event_based_hold    = false
+    force_destroy               = false
+    labels                      = {}
+    location                    = "AUSTRALIA-SOUTHEAST1"
+    name                        = "${var.bucket}_s3"
+    project                     = "ford-data-migration-poc"
+    requester_pays              = false
+    storage_class               = "STANDARD"
+    uniform_bucket_level_access = false
+}
+resource "google_storage_bucket" "s18" {
+    default_event_based_hold    = false
+    force_destroy               = false
+    labels                      = {}
+    location                    = "AUSTRALIA-SOUTHEAST1"
+    name                        = "${var.bucket}_s3"
+    project                     = "ford-data-migration-poc"
+    requester_pays              = false
+    storage_class               = "STANDARD"
+    uniform_bucket_level_access = false
+}
+resource "google_storage_bucket" "s19" {
+    default_event_based_hold    = false
+    force_destroy               = false
+    labels                      = {}
+    location                    = "AUSTRALIA-SOUTHEAST1"
+    name                        = "${var.bucket}_s3"
+    project                     = "ford-data-migration-poc"
+    requester_pays              = false
+    storage_class               = "STANDARD"
+    uniform_bucket_level_access = false
+}
+resource "google_storage_bucket" "s20" {
+    default_event_based_hold    = false
+    force_destroy               = false
+    labels                      = {}
+    location                    = "AUSTRALIA-SOUTHEAST1"
+    name                        = "${var.bucket}_s3"
+    project                     = "ford-data-migration-poc"
+    requester_pays              = false
+    storage_class               = "STANDARD"
+    uniform_bucket_level_access = false
+}
+resource "google_storage_bucket" "s21" {
+    default_event_based_hold    = false
+    force_destroy               = false
+    labels                      = {}
+    location                    = "AUSTRALIA-SOUTHEAST1"
+    name                        = "${var.bucket}_s3"
+    project                     = "ford-data-migration-poc"
+    requester_pays              = false
+    storage_class               = "STANDARD"
+    uniform_bucket_level_access = false
+}
+resource "google_storage_bucket" "s22" {
+    default_event_based_hold    = false
+    force_destroy               = false
+    labels                      = {}
+    location                    = "AUSTRALIA-SOUTHEAST1"
+    name                        = "${var.bucket}_s3"
+    project                     = "ford-data-migration-poc"
+    requester_pays              = false
+    storage_class               = "STANDARD"
+    uniform_bucket_level_access = false
+}
+resource "google_storage_bucket" "s23" {
+    default_event_based_hold    = false
+    force_destroy               = false
+    labels                      = {}
+    location                    = "AUSTRALIA-SOUTHEAST1"
+    name                        = "${var.bucket}_s3"
+    project                     = "ford-data-migration-poc"
+    requester_pays              = false
+    storage_class               = "STANDARD"
+    uniform_bucket_level_access = false
+}
+resource "google_storage_bucket" "s24" {
+    default_event_based_hold    = false
+    force_destroy               = false
+    labels                      = {}
+    location                    = "AUSTRALIA-SOUTHEAST1"
+    name                        = "${var.bucket}_s3"
+    project                     = "ford-data-migration-poc"
+    requester_pays              = false
+    storage_class               = "STANDARD"
+    uniform_bucket_level_access = false
+}
+resource "google_storage_bucket" "s25" {
+    default_event_based_hold    = false
+    force_destroy               = false
+    labels                      = {}
+    location                    = "AUSTRALIA-SOUTHEAST1"
+    name                        = "${var.bucket}_s3"
+    project                     = "ford-data-migration-poc"
+    requester_pays              = false
+    storage_class               = "STANDARD"
+    uniform_bucket_level_access = false
+}
+resource "google_storage_bucket" "s26" {
+    default_event_based_hold    = false
+    force_destroy               = false
+    labels                      = {}
+    location                    = "AUSTRALIA-SOUTHEAST1"
+    name                        = "${var.bucket}_s3"
+    project                     = "ford-data-migration-poc"
+    requester_pays              = false
+    storage_class               = "STANDARD"
+    uniform_bucket_level_access = false
+}
+resource "google_storage_bucket" "s27" {
+    default_event_based_hold    = false
+    force_destroy               = false
+    labels                      = {}
+    location                    = "AUSTRALIA-SOUTHEAST1"
+    name                        = "${var.bucket}_s3"
+    project                     = "ford-data-migration-poc"
+    requester_pays              = false
+    storage_class               = "STANDARD"
+    uniform_bucket_level_access = false
+}
+resource "google_storage_bucket" "s28" {
+    default_event_based_hold    = false
+    force_destroy               = false
+    labels                      = {}
+    location                    = "AUSTRALIA-SOUTHEAST1"
+    name                        = "${var.bucket}_s3"
+    project                     = "ford-data-migration-poc"
+    requester_pays              = false
+    storage_class               = "STANDARD"
+    uniform_bucket_level_access = false
+}
+resource "google_storage_bucket" "s29" {
+    default_event_based_hold    = false
+    force_destroy               = false
+    labels                      = {}
+    location                    = "AUSTRALIA-SOUTHEAST1"
+    name                        = "${var.bucket}_s3"
+    project                     = "ford-data-migration-poc"
+    requester_pays              = false
+    storage_class               = "STANDARD"
+    uniform_bucket_level_access = false
+}
+resource "google_storage_bucket" "s30" {
+    default_event_based_hold    = false
+    force_destroy               = false
+    labels                      = {}
+    location                    = "AUSTRALIA-SOUTHEAST1"
+    name                        = "${var.bucket}_s3"
+    project                     = "ford-data-migration-poc"
+    requester_pays              = false
+    storage_class               = "STANDARD"
+    uniform_bucket_level_access = false
+}
+resource "google_storage_bucket" "s31" {
+    default_event_based_hold    = false
+    force_destroy               = false
+    labels                      = {}
+    location                    = "AUSTRALIA-SOUTHEAST1"
+    name                        = "${var.bucket}_s3"
+    project                     = "ford-data-migration-poc"
+    requester_pays              = false
+    storage_class               = "STANDARD"
+    uniform_bucket_level_access = false
+}
+resource "google_storage_bucket" "s32" {
+    default_event_based_hold    = false
+    force_destroy               = false
+    labels                      = {}
+    location                    = "AUSTRALIA-SOUTHEAST1"
+    name                        = "${var.bucket}_s3"
+    project                     = "ford-data-migration-poc"
+    requester_pays              = false
+    storage_class               = "STANDARD"
+    uniform_bucket_level_access = false
+}
+resource "google_storage_bucket" "s33" {
+    default_event_based_hold    = false
+    force_destroy               = false
+    labels                      = {}
+    location                    = "AUSTRALIA-SOUTHEAST1"
+    name                        = "${var.bucket}_s3"
+    project                     = "ford-data-migration-poc"
+    requester_pays              = false
+    storage_class               = "STANDARD"
+    uniform_bucket_level_access = false
+}
+resource "google_storage_bucket" "s34" {
+    default_event_based_hold    = false
+    force_destroy               = false
+    labels                      = {}
+    location                    = "AUSTRALIA-SOUTHEAST1"
+    name                        = "${var.bucket}_s3"
+    project                     = "ford-data-migration-poc"
+    requester_pays              = false
+    storage_class               = "STANDARD"
+    uniform_bucket_level_access = false
+}
+resource "google_storage_bucket" "s35" {
+    default_event_based_hold    = false
+    force_destroy               = false
+    labels                      = {}
+    location                    = "AUSTRALIA-SOUTHEAST1"
+    name                        = "${var.bucket}_s3"
+    project                     = "ford-data-migration-poc"
+    requester_pays              = false
+    storage_class               = "STANDARD"
+    uniform_bucket_level_access = false
+}
+resource "google_storage_bucket" "s36" {
+    default_event_based_hold    = false
+    force_destroy               = false
+    labels                      = {}
+    location                    = "AUSTRALIA-SOUTHEAST1"
+    name                        = "${var.bucket}_s3"
+    project                     = "ford-data-migration-poc"
+    requester_pays              = false
+    storage_class               = "STANDARD"
+    uniform_bucket_level_access = false
+}
+resource "google_storage_bucket" "s37" {
+    default_event_based_hold    = false
+    force_destroy               = false
+    labels                      = {}
+    location                    = "AUSTRALIA-SOUTHEAST1"
+    name                        = "${var.bucket}_s3"
+    project                     = "ford-data-migration-poc"
+    requester_pays              = false
+    storage_class               = "STANDARD"
+    uniform_bucket_level_access = false
+}
+resource "google_storage_bucket" "s38" {
+    default_event_based_hold    = false
+    force_destroy               = false
+    labels                      = {}
+    location                    = "AUSTRALIA-SOUTHEAST1"
+    name                        = "${var.bucket}_s3"
+    project                     = "ford-data-migration-poc"
+    requester_pays              = false
+    storage_class               = "STANDARD"
+    uniform_bucket_level_access = false
+}
+resource "google_storage_bucket" "s39" {
+    default_event_based_hold    = false
+    force_destroy               = false
+    labels                      = {}
+    location                    = "AUSTRALIA-SOUTHEAST1"
+    name                        = "${var.bucket}_s3"
+    project                     = "ford-data-migration-poc"
+    requester_pays              = false
+    storage_class               = "STANDARD"
+    uniform_bucket_level_access = false
+}
+resource "google_storage_bucket" "s40" {
+    default_event_based_hold    = false
+    force_destroy               = false
+    labels                      = {}
+    location                    = "AUSTRALIA-SOUTHEAST1"
+    name                        = "${var.bucket}_s3"
+    project                     = "ford-data-migration-poc"
+    requester_pays              = false
+    storage_class               = "STANDARD"
+    uniform_bucket_level_access = false
+}
+resource "google_storage_bucket" "s41" {
+    default_event_based_hold    = false
+    force_destroy               = false
+    labels                      = {}
+    location                    = "AUSTRALIA-SOUTHEAST1"
+    name                        = "${var.bucket}_s3"
+    project                     = "ford-data-migration-poc"
+    requester_pays              = false
+    storage_class               = "STANDARD"
+    uniform_bucket_level_access = false
+}
+resource "google_storage_bucket" "s42" {
+    default_event_based_hold    = false
+    force_destroy               = false
+    labels                      = {}
+    location                    = "AUSTRALIA-SOUTHEAST1"
+    name                        = "${var.bucket}_s3"
+    project                     = "ford-data-migration-poc"
+    requester_pays              = false
+    storage_class               = "STANDARD"
+    uniform_bucket_level_access = false
+}
+resource "google_storage_bucket" "s43" {
+    default_event_based_hold    = false
+    force_destroy               = false
+    labels                      = {}
+    location                    = "AUSTRALIA-SOUTHEAST1"
+    name                        = "${var.bucket}_s3"
+    project                     = "ford-data-migration-poc"
+    requester_pays              = false
+    storage_class               = "STANDARD"
+    uniform_bucket_level_access = false
+}
+resource "google_storage_bucket" "s44" {
+    default_event_based_hold    = false
+    force_destroy               = false
+    labels                      = {}
+    location                    = "AUSTRALIA-SOUTHEAST1"
+    name                        = "${var.bucket}_s3"
+    project                     = "ford-data-migration-poc"
+    requester_pays              = false
+    storage_class               = "STANDARD"
+    uniform_bucket_level_access = false
+}
